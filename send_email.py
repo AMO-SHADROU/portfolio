febf283ba15bd73f0017396677e3f5b81ae6eb9d
AMO-SHADROU <106986563+AMO-SHADROU@users.noreply.github.com>
2023-12-21 21:05:13 +0330

    make email pass secure.

--- send_email.py
+++ send_email.py
@@ -1,12 +1,13 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(user_massage):
     host = 'smtp.gmail.com'
     port = 465
     gmail = "amirali.shadrou@gmail.com"
-    password = "gmxfzhfxuogtonoq"
+    password = os.getenv("PASSWORD")
     receiver = "dr.shadrou@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
